6ef8db9558cb6c4ceef6fb5188cddd5d47ae87e8
cozin01 <121503067+cozin01@users.noreply.github.com>
2023-05-08 16:44:33 -0300

    Update ejericio2-6-9.hs
    arreglados problemas de sintaxis; cambiado los nombres de auxiliares del ej 9.

--- ejericio2-6-9.hs
+++ ejericio2-6-9.hs
@@ -4,8 +4,8 @@ amigosDe red us = aux_amigosDe (relaciones red) us
 
 aux_amigosDe :: [Relacion] -> Usuario -> [Usuario]
 aux_amigosDe [] _ = []
-aux_amigosDe (rel:rels) us | primero == us = segundo : aux_amigosDe rels us
-                           | segundo == us = primero : aux_amigosDe rels us  -- si ninguno se cumple, es que no era relacion del us -- 
+aux_amigosDe (rel:rels) us | (primero rel) == us = (segundo rel) : (aux_amigosDe rels us)
+                           | (segundo rel) == us = (primero rel) : (aux_amigosDe rels us)  -- si ninguno se cumple, es que no era relacion del us -- 
                            | otherwise =  aux_amigosDe rels us 
 
 primero :: (t,t) -> t
@@ -15,6 +15,7 @@ segundo :: (t,t) -> t
 segundo (x, y) = y
 
 -- ejercicio 6 -- 
+
 publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
 publicacionesDe red us = aux_publicacionesDe (publicaciones red) us  
 
@@ -23,26 +24,25 @@ aux_publicacionesDe [] _ = []
 aux_publicacionesDe (pub:pubs) us | us == usuarioDePublicacion pub = pub : aux_publicacionesDe pubs us 
                                   | otherwise = aux_publicacionesDe pubs us
 
-
 -- ejercicio 9 -- 
 tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
-tieneUnSeguidorFiel red us = aux1_tieneUnSeguidorFiel (publicacionesDe red us) 
+tieneUnSeguidorFiel red us = aux_tieneSegFiel (publicacionesDe red us) 
 
-aux1_tieneUnSeguidorFiel :: [Publicacion] -> Bool --(pub:pubs) es la lista de publicaciones de us
-aux1_tieneUnSeguidorFiel [] = False
-aux1_tieneUnSeguidorFiel (pub:pubs) = aux2_tieneUnSeguidorFiel (eliminarRepetidos (likesDePublicacion pub)) (concatenarLikesDePublicaciones (pub:pubs)) (pub:pubs) 
+aux_tieneSegFiel :: [Publicacion] -> Bool -- pubs es la lista de publicaciones de us
+aux_tieneSegFiel [] = False
+aux_tieneSegFiel pubs = aparicionesDeLikeador (eliminarRepetidos (likesDePublicacion (head pubs))) (concatenarLikesDePublicaciones pubs) (longitud pubs) 
 
-aux2_tieneUnSeguidorFiel :: [Usuario] -> [Usuario] -> [Publicacion] -> Bool -- usl1 son los usuarios que le dieron like a la primera publicacion, usl2 son los usuario de la concatenarLikesDePublicaciones. -- 
-aux2_tieneUnSeguidorFiel [] _ _ = False -- si nadie dio like en la primera publicacion, entonces False -- 
-aux2_tieneUnSeguidorFiel _ [] _ = False -- si concateno todos los likes y es vacío, es que nadie dio like = False
-aux2_tieneUnSeguidorFiel  _ _ [] = False -- si no tenia ninguna publicacion en la red = Falso -- 
-aux2_tieneUnSeguidorFiel (usl1:ussl1) likesTotales pubsDeUser | cantidadDeApariciones (usl1) likesTotales == longitud pubsDeUser = True -- likesTotales es la lista concatenarLikesDePublicaciones -- 
-                                                              | otherwise = aux2_tieneUnSeguidorFiel ussl1 likesTotales pubsDeUser 
+aparicionesDeLikeador :: [Usuario] -> [Usuario] -> Int -> Bool -- usl1 son los usuarios que le dieron like a la primera publicacion, usl2 son los usuario de la concatenarLikesDePublicaciones. -- 
+aparicionesDeLikeador [] _ _ = False -- si nadie dio like en la primera publicacion, entonces False -- 
+aparicionesDeLikeador _ [] _ = False -- si concateno todos los likes y es vacío, es que nadie dio like = False
+aparicionesDeLikeador  _ _ [] = False -- si no tenia ninguna publicacion en la red = Falso -- 
+aparicionesDeLikeador (usl1:ussl1) likesTotales pubsTotales | (cantidadDeApariciones usl1 likesTotales) == pubsTotales = True -- likesTotales es la lista concatenarLikesDePublicaciones -- 
+                                                              | otherwise = aparicionesDeLikeador ussl1 likesTotales pubsTotales 
 
 cantidadDeApariciones :: Usuario -> [Usuario] -> Int -- usX es un usuario genérico -- 
 cantidadDeApariciones _ [] = 0
-cantidadDeApariciones usX (us:users) | usX == us = 1 + cantidadDeApariciones usX uss 
-                                   | otherwise = cantidadDeApariciones usX uss 
+cantidadDeApariciones usX (us:users) | usX == us = 1 + (cantidadDeApariciones usX users) 
+                                     | otherwise = cantidadDeApariciones usX users 
 
 concatenarLikesDePublicaciones :: [Publicacion] -> [Usuario] -- agarro los likes de la primera publicación y los concateno, luego hago recursión sobre la lista sin la primera publicación, y así... --
 concatenarLikesDePublicaciones [] = []
