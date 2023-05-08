5f9ef7b397b6022aeb0a4586d12fc3cb78befaa4
cozin01 <121503067+cozin01@users.noreply.github.com>
2023-05-08 15:54:59 -0300

    Update ejericio6y9.hs
    Cambio orden de aparición de ejercicios.
    Renombradas y reimplementadas "primerElemRelacion" "segundoElemRelacion".
    Eliminados los ejs 3 y 4 (están en su propio archivo).

--- ejericio6y9.hs
+++ ejericio6y9.hs
@@ -1,35 +1,18 @@
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+-- 2 -- 
+amigosDe :: RedSocial -> Usuario -> [Usuario]
+amigosDe red us = aux_amigosDe (relaciones red) us
 
+aux_amigosDe ::[Relacion] -> Usuario -> [Usuario]
+aux_amigosDe [] _ = []
+aux_amigosDe (rel:rels) us | primero == us = (segundoElemDeRelacion rel):aux_amigosDe rels us
+                           | segundo == us = (primerElemDeRelacion rel):aux_amigosDe rels us  -- si ninguno se cumple, es que no era relacion del us -- 
+                           | otherwise =  aux_amigosDe rels us 
 
-type Usuario = (Integer, String) -- (id, nombre)
-type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
-type Publicacion = (Usuario, String, [Usuario]) -- (usuario que publica, texto publicacion, likes)
-type RedSocial = ([Usuario], [Relacion], [Publicacion])
+primero :: (t,t) -> t
+primero (x, y) = x
 
-
-
--- Funciones basicas
-
-usuarios :: RedSocial -> [Usuario]
-usuarios (us, _, _) = us
-
-relaciones :: RedSocial -> [Relacion]
-relaciones (_, rs, _) = rs
-
-publicaciones :: RedSocial -> [Publicacion]
-publicaciones (_, _, ps) = ps
-
-idDeUsuario :: Usuario -> Integer
-idDeUsuario (id, _) = id 
-
-nombreDeUsuario :: Usuario -> String
-nombreDeUsuario (_, nombre) = nombre 
-
-usuarioDePublicacion :: Publicacion -> Usuario
-usuarioDePublicacion (u, _, _) = u
-
-likesDePublicacion :: Publicacion -> [Usuario]
-likesDePublicacion (_, _, us) = us
+segundo :: (t,t) -> t
+segundo (x, y) = y
 
 -- ejercicio 6 -- 
 publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
@@ -110,89 +93,3 @@ sacarSiPrimerEsRepetido (x:xs) | not (pertenece x xs) = (x:xs) -- en realidad si
                         | pertenece x xs = [x] -- si el primero es repetido, lo separa en una lista con el repetido como único enlemento -- 
                         | otherwise = sacarSiPrimerEsRepetido xs               
 
--- 2 -- 
-amigosDe :: RedSocial -> Usuario -> [Usuario]
-amigosDe red us = aux_amigosDe (relaciones red) us
-
-aux_amigosDe ::[Relacion] -> Usuario -> [Usuario]
-aux_amigosDe [] _ = []
-aux_amigosDe (rel:rels) us | (primerElemDeRelacion rel) == us = (segundoElemDeRelacion rel):aux_amigosDe rels us
-                           | segundoElemDeRelacion rel == us = (primerElemDeRelacion rel):aux_amigosDe rels us  -- si ninguno se cumple, es que no era relacion del us -- 
-                           | otherwise =  aux_amigosDe rels us 
-
-primerElemListaDeRelaciones :: [Relacion] -> Relacion
-primerElemListaDeRelaciones (rel:rels) = rel 
-
-primerElemDeRelacion :: Relacion -> Usuario
-primerElemDeRelacion (usuarioX, usuarioY) = usuarioX
-
-segundoElemDeRelacion :: Relacion -> Usuario
-segundoElemDeRelacion (usuarioX, usuarioY) = usuarioY
-
--- 3 -- 
-cantidadDeAmigos :: RedSocial -> Usuario -> Int
-cantidadDeAmigos red us  =  longitud (amigosDe (red) us)
--- 4 --
-usuarioConMasAmigos :: RedSocial -> Usuario
-usuarioConMasAmigos red = buscarUsuarioMax red (x:xs) m 
-  where
-    (x:xs) = usuarios red 
-    m = maximo (listaCantDeAmigos red (x:xs))
-
-buscarUsuarioMax :: RedSocial -> [Usuario] -> Int -> Usuario    
-buscarUsuarioMax red (x:xs) m | cantidadDeAmigos red x == m = x     
-                              | otherwise = buscarUsuarioMax red xs m
-
-listaCantDeAmigos :: RedSocial -> [Usuario] -> [Int]
-listaCantDeAmigos red [] = []  
-listaCantDeAmigos red (x:xs) = cantidadDeAmigos red x : listaCantDeAmigos red xs 
-
-maximo :: [Int] -> Int
-maximo (x:[]) = x 
-maximo (x:xs) | x > head xs = mayor x (maximo xs)
-              | otherwise = mayor (head xs) (maximo xs)
-
-mayor :: Int -> Int -> Int 
-mayor x y | x >= y = x 
-          | otherwise = y 
--- Ejemplos
-
-usuario1 = (1, "Juan")
-usuario2 = (2, "Natalia")
-usuario3 = (3, "Pedro")
-usuario4 = (4, "Mariela")
-usuario5 = (5, "Natalia")
-
-relacion1_2 = (usuario1, usuario2)
-relacion1_3 = (usuario1, usuario3)
-relacion1_4 = (usuario4, usuario1) -- Notar que el orden en el que aparecen los usuarios es indistinto
-relacion2_3 = (usuario3, usuario2)
-relacion2_4 = (usuario2, usuario4)
-relacion3_4 = (usuario4, usuario3)
-
-publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])
-publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
-publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
-publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
-publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
-
-publicacion2_1 = (usuario2, "Hello World", [usuario4])
-publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
-
-publicacion3_1 = (usuario3, "Lorem Ipsum", [])
-publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
-publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
-
-publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
-publicacion4_2 = (usuario4, "I am Bob", [])
-publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
-
-
-usuariosA = [usuario1, usuario2, usuario3, usuario4]
-relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4]
-publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2]
-redA = (usuariosA, relacionesA, publicacionesA)
- 
-
-
- 
