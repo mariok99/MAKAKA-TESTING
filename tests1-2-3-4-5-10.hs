f084c12ea4355f3adbef4fcdd80d7a6f8d295589
cozin01 <121503067+cozin01@users.noreply.github.com>
2023-05-10 15:56:40 -0300

    Update tests1-2-3-4-5-10.hs
    Typo con ,s

--- tests1-2-3-4-5-10.hs
+++ tests1-2-3-4-5-10.hs
@@ -43,43 +43,43 @@ tests = test [testSuiteEj1,testSuiteEj2,testSuiteEj3,testSuiteEj4,testSuiteEj5,t
 run1 = runTestTT tests
 
 testSuiteEj1 = test [
-  " Caso 1: lista usuario vacía" ~: (nombresDeUsuarios redUsVacio) ~?= []
-  " Caso 2: lista de usuarios sin nombres repetidos" ~: (nombresDeUsuarios redJ1) ~?= ["Marcelo","Pepe","Ronaldo"]
+  " Caso 1: lista usuario vacía" ~: (nombresDeUsuarios redUsVacio) ~?= [],
+  " Caso 2: lista de usuarios sin nombres repetidos" ~: (nombresDeUsuarios redJ1) ~?= ["Marcelo","Pepe","Ronaldo"],
   " Caso 3: lista de usuarios con nombres repetidos" ~: (nombresDeUsuarios redNomRep) ~?= ["Marcelo","Carlos"]
  ]
 
 
 
 testSuiteEj2 = test [
-  " Caso 1: lista relaciones vacía" ~: (amigosDe redRelsVacio usJ1) ~?= []
-  " Caso 2: usuario no tiene amigos" ~: (amigosDe redJ1 usJ7) ~?= []
+  " Caso 1: lista relaciones vacía" ~: (amigosDe redRelsVacio usJ1) ~?= [],
+  " Caso 2: usuario no tiene amigos" ~: (amigosDe redJ1 usJ7) ~?= [],
   " Caso 3: usuario tiene amigos" ~: (amigosDe redJ2 usJ4) ~?= [usJ1,usJ2,usJ7]
  ]
 
 testSuiteEj3 = test [
-  " Caso 1: lista relaciones vacía" ~: (cantidadDeAmigos redRelsVacio usJ1) ~?= 0
-  " Caso 2: usuario no tiene amigos" ~: (cantidadDeAmigos redJ1 usJ7) ~?= 0
+  " Caso 1: lista relaciones vacía" ~: (cantidadDeAmigos redRelsVacio usJ1) ~?= 0,
+  " Caso 2: usuario no tiene amigos" ~: (cantidadDeAmigos redJ1 usJ7) ~?= 0,
   " Caso 3: usuario tiene amigos" ~: (cantidadDeAmigos redJ2 usJ2) ~?= 2
  ]
 
 testSuiteEj4 = test [
-  " Caso 1: lista relaciones vacía" ~: (usuarioConMasAmigos redRelsVacio) ~?= usJ1
-  " Caso 2: un único usuario con máximo de amigos" ~: (usuarioConMasAmigos redJ2) ~?= usJ4
+  " Caso 1: lista relaciones vacía" ~: (usuarioConMasAmigos redRelsVacio) ~?= usJ1,
+  " Caso 2: un único usuario con máximo de amigos" ~: (usuarioConMasAmigos redJ2) ~?= usJ4,
   " Caso 3: varios usuarios con el máximo de amigos" ~: (usuarioConMasAmigos redJ3) ~?= usJ2
  ]
 
 testSuiteEj5 = test [
-  " Caso 1: No hay usuario con más de 10 amigos" ~: (estaRobertoCarlos redJ1) ~?= False
+  " Caso 1: No hay usuario con más de 10 amigos" ~: (estaRobertoCarlos redJ1) ~?= False,
   " Caso 2: Hay usuario con más de 10 amigos" ~: (estaRobertoCarlos redRobertoTrue) ~?= True
  ]
 
 testSuiteEj10 = test [
-  " Caso 1: usuarios de entrada iguales, sin amigos" ~: (existeSecuenciaDeAmigos redJ1 usJ7 usJ7) ~?= False
-  " Caso 2: usuarios de entrada iguales, con amigos" ~: (existeSecuenciaDeAmigos redJ1 usJ1 usJ1) ~?= True
-  " Caso 3: usuarios de entrada distintos, sin amigos" ~: (existeSecuenciaDeAmigos redJ4 usJ4 usJ11) ~?= False
-  " Caso 4: primer usuario no tiene amigos" ~: (existeSecuenciaDeAmigos redJ4 usJ4 usJ1) ~?= False
-  " Caso 5: segundo usuario no tiene amigos" ~: (existeSecuenciaDeAmigos redJ4 usJ1 usJ11) ~?= False
-  " Caso 6: ambos usuarios tienen amigos, pero no están relacionados" ~: (existeSecuenciaDeAmigos redJ4 usJ1 usJ2) ~?= False
-  " Caso 7: ambos usuarios son amigos entre sí" ~: (existeSecuenciaDeAmigos redJ4 usJ2 usJ5) ~?= True
+  " Caso 1: usuarios de entrada iguales, sin amigos" ~: (existeSecuenciaDeAmigos redJ1 usJ7 usJ7) ~?= False,
+  " Caso 2: usuarios de entrada iguales, con amigos" ~: (existeSecuenciaDeAmigos redJ1 usJ1 usJ1) ~?= True,
+  " Caso 3: usuarios de entrada distintos, sin amigos" ~: (existeSecuenciaDeAmigos redJ4 usJ4 usJ11) ~?= False,
+  " Caso 4: primer usuario no tiene amigos" ~: (existeSecuenciaDeAmigos redJ4 usJ4 usJ1) ~?= False,
+  " Caso 5: segundo usuario no tiene amigos" ~: (existeSecuenciaDeAmigos redJ4 usJ1 usJ11) ~?= False,
+  " Caso 6: ambos usuarios tienen amigos, pero no están relacionados" ~: (existeSecuenciaDeAmigos redJ4 usJ1 usJ2) ~?= False,
+  " Caso 7: ambos usuarios son amigos entre sí" ~: (existeSecuenciaDeAmigos redJ4 usJ2 usJ5) ~?= True,
   " Caso 8: ambos usuarios están relacionados lejanamente" ~: (existeSecuenciaDeAmigos redJ3 usJ1 usJ11) ~?= True
  ]
