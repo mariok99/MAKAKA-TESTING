ac4a5ed16edefa346fc040386c05a6e11b80de7e
cozin01 <121503067+cozin01@users.noreply.github.com>
2023-05-10 15:54:57 -0300

    Update tests1-2-3-4-5-10.hs
    Typos y errores

--- tests1-2-3-4-5-10.hs
+++ tests1-2-3-4-5-10.hs
@@ -1,3 +1,6 @@
+import Solucion
+import Test.HUnit
+
 usJ1=(1,"Marcelo")
 usJ2=(2,"Carlos")
 usJ3=(3,"Pepe")
@@ -9,8 +12,9 @@ usJ8=(8,"Angela")
 usJ9=(9,"Fulana")
 usJ10=(10,"Messi")
 usJ11=(11,"Leopoldo")
+usJ12=(12,"Alfredo")
 
-usNomRep=(12,"Marcelo")
+usNomRep=(777,"Marcelo")
 
 ususariosRobertoTrue=[usJ1,usJ2,usJ3,usJ4,usJ5,usJ6,usJ7,usJ8,usJ9,usJ10,usJ11,usJ12]
 usuariosJ1=[usJ1,usJ3,usJ7]
@@ -41,33 +45,33 @@ run1 = runTestTT tests
 testSuiteEj1 = test [
   " Caso 1: lista usuario vacía" ~: (nombresDeUsuarios redUsVacio) ~?= []
   " Caso 2: lista de usuarios sin nombres repetidos" ~: (nombresDeUsuarios redJ1) ~?= ["Marcelo","Pepe","Ronaldo"]
-  " Caso 3: lista de usuarios con nombres repetidos" ~: (nombresDeUsuarios redNomRep) ~?= ["Marcelo","Roberto Carlos"]
-]
+  " Caso 3: lista de usuarios con nombres repetidos" ~: (nombresDeUsuarios redNomRep) ~?= ["Marcelo","Carlos"]
+ ]
 
 
 
 testSuiteEj2 = test [
-  " Caso 1: lista relaciones vacía" ~: (amigosDe redJ1 usJ1) ~?= []
+  " Caso 1: lista relaciones vacía" ~: (amigosDe redRelsVacio usJ1) ~?= []
   " Caso 2: usuario no tiene amigos" ~: (amigosDe redJ1 usJ7) ~?= []
   " Caso 3: usuario tiene amigos" ~: (amigosDe redJ2 usJ4) ~?= [usJ1,usJ2,usJ7]
-]
+ ]
 
 testSuiteEj3 = test [
   " Caso 1: lista relaciones vacía" ~: (cantidadDeAmigos redRelsVacio usJ1) ~?= 0
   " Caso 2: usuario no tiene amigos" ~: (cantidadDeAmigos redJ1 usJ7) ~?= 0
   " Caso 3: usuario tiene amigos" ~: (cantidadDeAmigos redJ2 usJ2) ~?= 2
-]
+ ]
 
 testSuiteEj4 = test [
   " Caso 1: lista relaciones vacía" ~: (usuarioConMasAmigos redRelsVacio) ~?= usJ1
   " Caso 2: un único usuario con máximo de amigos" ~: (usuarioConMasAmigos redJ2) ~?= usJ4
   " Caso 3: varios usuarios con el máximo de amigos" ~: (usuarioConMasAmigos redJ3) ~?= usJ2
-]
+ ]
 
 testSuiteEj5 = test [
   " Caso 1: No hay usuario con más de 10 amigos" ~: (estaRobertoCarlos redJ1) ~?= False
   " Caso 2: Hay usuario con más de 10 amigos" ~: (estaRobertoCarlos redRobertoTrue) ~?= True
-]
+ ]
 
 testSuiteEj10 = test [
   " Caso 1: usuarios de entrada iguales, sin amigos" ~: (existeSecuenciaDeAmigos redJ1 usJ7 usJ7) ~?= False
@@ -78,4 +82,4 @@ testSuiteEj10 = test [
   " Caso 6: ambos usuarios tienen amigos, pero no están relacionados" ~: (existeSecuenciaDeAmigos redJ4 usJ1 usJ2) ~?= False
   " Caso 7: ambos usuarios son amigos entre sí" ~: (existeSecuenciaDeAmigos redJ4 usJ2 usJ5) ~?= True
   " Caso 8: ambos usuarios están relacionados lejanamente" ~: (existeSecuenciaDeAmigos redJ3 usJ1 usJ11) ~?= True
-]
+ ]
